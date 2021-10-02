2b5260d47b03198378ba479969113184ce6f6254
Saifullina <tgvmironova@gmail.com>
2021-10-02 12:31:23 +0300

    lesson_3

---
+++ lesson_3/index.js
@@ -0,0 +1,98 @@
+// STRING
+// const firstValue = String(5);
+
+// console.log(typeof firstValue);
+
+// const obj = {
+//     foo: 67,
+// };
+
+// console.log(typeof String(obj.foo));
+
+// NUMBER
+
+// let testValue;
+
+// console.log(typeof testValue);
+
+// testValue = Number(testValue);
+
+// console.log(testValue);
+
+
+// const isUser=true;
+// const isAdmin=false;
+
+// console.log(Number(isUser),Number(isAdmin));
+
+// const voidString="";
+// console.log(Number(voidString));
+
+
+
+
+// BOOLEAN
+
+// const value="    ";
+
+// console.log(typeof Boolean(value), Boolean(value));
+
+
+// const obj=[ ].length;
+// console.log(Boolean(obj));
+
+
+// МАТЕМАТИЧЕСКИЕ ОПЕРАТОРЫ
+
+// console.log(5%2);
+// console.log(2**2);
+
+// Унарный плюс
+
+// const value="5";
+
+// console.log(typeof value, typeof+value);
+
+// Бинарный плюс
+
+// let sum=5+6;
+// console.log(sum);
+
+// sum=1+true;
+// console.log(sum);
+
+// sum=5+null;
+// console.log(sum);
+
+// const stringValue="12";
+// const numberValue=34;
+
+// console.log(+stringValue+numberValue);
+// console.log(numberValue + +stringValue);
+// console.log(typeof (numberValue + + stringValue));
+
+
+// const firstName="Alex";
+// const lastName="Shirokov";
+
+// console.log(firstName + " " + lastName);
+// console.log(`${firstName} ${lastName}`); 
+
+
+// Операции сравнения
+
+// console.log(false ==0);
+
+// const firstString="яблоко";
+// const secondString="Яблоко";
+
+// console.log(firstString!=secondString);
+
+// Практика
+
+console.log("2">"12");
+console.log(null=="\n0\n");
+console.log(null===+"\n0\n");
+console.log(""-1+0, typeof(""-1+0));
+console.log(true+" "+false);
+console.log(6/"3");
